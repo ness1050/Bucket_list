5836627ed0f9d336bc3ceae42ed84a1da8500ba0
Naseem Qurbanli <nq222af@student.lnu.se>
2025-07-17 14:59:42 +0200

    rendering Dreams functions built up

--- src/script/Dashboard.ts
+++ src/script/Dashboard.ts
@@ -6,7 +6,37 @@ const Username = document.getElementById("user-name") as HTMLElement;
 
 Username.innerHTML = `<p> ${username} </p>`;
 
-const dreamArray: Dream[] = [];
+let dreams: Dream[] = loadDreams();
+
+function loadDreams(): void {
+    const storedDreams = localStorage.getItem("dreams");
+    return storedDreams ? JSON.parse(storedDreams) : [];
+}
+
+function saveDream() : void {
+    localStorage.setItem("dream", JSON.stringify(dreams));
+}
+
+function renderDreams(): void {
+    let dreamList = document.querySelector(".dream-list") as HTMLUListElement;
+    if(!dreamList) {
+        const section = document.getElementById("main section") as HTMLElement;
+        const ulList = document.createElement("ul");
+        ulList.classList.add("dream-list");
+        section.appendChild(ulList);
+        dreamList = ulList;
+    }
+
+    dreamList.innerHTML = "";
+
+    dreams.forEach((dream) => {
+        const li = document.createElement("li");
+        li.classList.add("dream-list_item");
+        li.innerHTML = `
+        `
+    })
+}
+
 
 // We need to create add item or product
 // we need an array of products to save the product into, using localstorage.
